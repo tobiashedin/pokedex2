f2eee4b1f5be38e6abe3b283f73772293dd25259
unknown <tobias.hedin@gmail.com>
2019-04-18 11:15:11 +0200

    Updates

---
+++ precache-manifest.34b0131f6d6b758ff772ae8b87068098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0671a69d9afc7fe57be377e88838a57d",
+    "url": "/pokedex2/static/media/spinner.0671a69d.gif"
+  },
+  {
+    "revision": "86128f831b5ce8c27bcf",
+    "url": "/pokedex2/static/js/runtime~main.93404200.js"
+  },
+  {
+    "revision": "187210757698af09fbd3",
+    "url": "/pokedex2/static/js/main.d645f7e7.chunk.js"
+  },
+  {
+    "revision": "14289e33118064dd12d0",
+    "url": "/pokedex2/static/js/2.5bb1712f.chunk.js"
+  },
+  {
+    "revision": "187210757698af09fbd3",
+    "url": "/pokedex2/static/css/main.54205062.chunk.css"
+  },
+  {
+    "revision": "14289e33118064dd12d0",
+    "url": "/pokedex2/static/css/2.e29d0255.chunk.css"
+  },
+  {
+    "revision": "38f1ef8e649d653822e2628c041a16eb",
+    "url": "/pokedex2/index.html"
+  }
+];
